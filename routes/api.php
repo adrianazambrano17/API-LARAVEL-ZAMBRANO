2e3f15be1281044efc012457882eefb1ab92dba5
adrianazambrano17 <adriana.zambranom@ug.edu.ec>
2022-03-02 02:14:23 -0500

    API-LARAVEL-ZAMBRANO
    Adriana Zambrano

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+Use App\Models\Article;
+
+Route::get('articles', function() {
+    
+    return Article::all();
+});
+
+Route::get('articles/{id}', function($id) {
+    return Article::find($id);
+});
+
+Route::post('articles', function(Request $request) {
+    return Article::create($request->all);
+});
+
+Route::put('articles/{id}', function(Request $request, $id) {
+    $article = Article::findOrFail($id);
+    $article->update($request->all());
+
+    return $article;
+});
+
+Route::delete('articles/{id}', function($id) {
+    Article::find($id)->delete();
+
+    return 204;
+});
